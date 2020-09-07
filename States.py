8248f2be254ea93223a76a4584efb97ad7f350b2
JHopple76 <70179411+JHopple76@users.noreply.github.com>
2020-09-07 13:55:29 -0400

    Add files via upload

---
+++ States.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd 
+Location = "States.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
